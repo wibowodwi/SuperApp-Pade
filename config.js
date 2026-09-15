0b312b72c1e73ce7a1898c7d13404e2d5c49dcf2
Wibowo Dwi Suwito <50950100+wibowodwi@users.noreply.github.com>
2026-09-15 13:34:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ window.SHOPMATE_CONFIG = {
   supabaseUrl: 'https://cmvwpwaipemdsjeayucp.supabase.co',
   supabaseAnonKey: 'sb_publishable_wAG9WXT44QOkIyxG0HO6OQ_W7VB2fRw',
   whatsappNumber: '6285156069175',
+  instagramUrl: 'https://www.instagram.com/',
+  tiktokUrl: 'https://www.tiktok.com/',
+  googleMapsUrl: 'https://www.google.com/maps/search/?api=1&query=Puri+Cipageran+Indah+1+B+No.18%2C+Cimahi+Utara%2C+Kota+Cimahi+40511',
   storeName: 'Toko Pade'
 };
